0bac742d7358f0a94b970ef17dcb6a751df242f5
yurdiansyah <yurdiansyah143@gmail.com>
2023-06-08 21:17:36 +0700

    update

--- resources/views/Dashboard/post/create.blade.php
+++ resources/views/Dashboard/post/create.blade.php
@@ -32,12 +32,6 @@
                     @endforeach
                 </select>
             </div>
-            <div class="mb-3">
-                <label for="image" class="form-label">Upload Gambar</label>
-                <img class="img-preview img-fluid">
-                <input class="form-control @error('image')  is-invalid @enderror" type="file" id="image" name="image" onchange="preview()">
-                @error('image')<div class ="invalid-feedback">{{ $message }}</div>@enderror
-            </div>
 
             <div class="mb-3">
                 <label for="body" class="form-label">Body</label>
